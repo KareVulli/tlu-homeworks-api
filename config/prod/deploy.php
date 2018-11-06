6c5577e7d0495186f9604c2b154cf26ca95b0731
Marvin Helstein <m_helstein@hotmail.com>
2018-11-07 00:56:08 +0200

    Added dotenv for dev environments

--- config/prod/deploy.php
+++ config/prod/deploy.php
@@ -10,9 +10,9 @@ return new class extends DefaultDeployer
             // SSH connection string to connect to the remote server (format: user@host-or-IP:port-number)
             ->server('j2rvaservud')
             // the absolute path of the remote server directory where the project is deployed
-            ->deployDir('/var/www/vhosts/tluapi/')
+            ->deployDir('/var/www/vhosts/tluapi')
             // the URL of the Git repository where the project code is hosted
-            ->repositoryUrl('https://github.com/KareVulli/tlu-homeworks-api')
+            ->repositoryUrl('git@github.com:KareVulli/tlu-homeworks-api.git')
             // the repository branch to deploy
             ->repositoryBranch('master')
         ;
@@ -21,13 +21,13 @@ return new class extends DefaultDeployer
     // run some local or remote commands before the deployment is started
     public function beforeStartingDeploy()
     {
-        // $this->runLocal('./vendor/bin/simple-phpunit');
+        // $this->runLocal('source ~/.bashrc');
     }
 
     // run some local or remote commands after the deployment is finished
     public function beforeFinishingDeploy()
     {
         // $this->runRemote('{{ console_bin }} app:my-task-name');
-        // $this->runLocal('say "The deployment has finished."');
+        $this->runLocal('say "The deployment has finished."');
     }
 };
